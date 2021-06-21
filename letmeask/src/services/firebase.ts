814d24b878350b54ee4969eb4b019216a83b9c24
diegohfcelestino <diegohfcelestino@gmail.com>
2021-06-21 16:21:37 -0300

    Configurações de ambiente para não mostrar as credenciais para conectar no firebase

--- letmeask/src/services/firebase.ts
+++ letmeask/src/services/firebase.ts
@@ -4,17 +4,16 @@ import "firebase/auth";
 import "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCRBhp8zqHrotL6op4X8uqWYrr41jDgnBM",
-  authDomain: "letmeask-47b7b.firebaseapp.com",
-  databaseURL: "https://letmeask-47b7b-default-rtdb.firebaseio.com",
-  projectId: "letmeask-47b7b",
-  storageBucket: "letmeask-47b7b.appspot.com",
-  messagingSenderId: "324210055883",
-  appId: "1:324210055883:web:7084c15a2d379846ccd94e",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
-
 export const database = firebase.database();
