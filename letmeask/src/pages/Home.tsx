31dda441aed8e0475d284558ad31e454c37cc88d
diegohfcelestino <diegohfcelestino@gmail.com>
2021-06-22 16:02:43 -0300

    Finalizado a autenticação e salvando no estado com useEffect, ajustado o primeiro hook useAuth, para otimizar as importações

--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -1,18 +1,22 @@
-import {useHistory} from 'react-router-dom'
+import { useHistory } from "react-router-dom";
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 import "../styles/auth.scss";
 import { Button } from "../components/Button";
+import { useAuth } from "../hooks/useAuth";
 
 export function Home() {
   const history = useHistory();
 
-  function handleCreateRoom() {
+  const { user, signInWithGoogle } = useAuth();
 
-    
-    history.push('/rooms/new');
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
 
+    history.push("/rooms/new");
   }
 
   return (
