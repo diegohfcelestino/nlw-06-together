cd90852a65bca8974b9e710ecdd5734ab9aa300a
diegohfcelestino <diegohfcelestino@gmail.com>
2021-06-21 22:18:47 -0300

    Instalado o react-router-dmo e configurado a navegação da Rome para a NewRoom

--- letmeask/src/pages/NewRoom.tsx
+++ letmeask/src/pages/NewRoom.tsx
@@ -1,8 +1,8 @@
+import {Link} from 'react-router-dom';
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
-import googleIconImg from "../assets/images/google-icon.svg";
-import "../styles/auth.scss";
 import { Button } from "../components/Button";
+import "../styles/auth.scss";
 
 export function NewRoom() {
   return (
@@ -24,7 +24,7 @@ export function NewRoom() {
             <Button type="submit">Criar sala</Button>
           </form>
           <p>
-            Quer entrar em uma sala existente? <a href="#">Clique aqui</a>
+            Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>
           </p>
         </div>
       </main>
